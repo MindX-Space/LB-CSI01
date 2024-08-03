acf13d1ac7a71169a9c82e4111387a39081b4f2c
whis102 <chicongnguyen.dev@gmail.com>
2024-08-03 14:56:43 +0700

    update

---
+++ B4/exercises.py
@@ -0,0 +1,23 @@
+"""
+    Bài 1: Tìm kiếm một phần tử trong mảng
+        Bài toán: Cho một mảng đã sắp xếp, tìm kiếm một phần tử trong mảng đó.
+        Giải pháp: Sử dụng thuật toán Binary Search để tìm kiếm nhanh chóng.
+        VD: arr = [1, 3, 5, 7, 9, 11, 13]
+            target = 5
+
+    Bài 2: Tìm kiếm tất cả các lần xuất hiện của một phần tử trong mảng
+        Bài toán: Cho một mảng, tìm tất cả các vị trí xuất hiện của một phần tử trong mảng.
+        Giải pháp: Sử dụng Linear Search để duyệt qua toàn bộ mảng và ghi nhận các vị trí xuất hiện.
+        VD: arr = [1, 3, 7, 8, 7, 5, 6, 7, 2]
+            target = 5
+
+    Bài 3: Tìm kiếm phần tử lớn nhất trong mảng
+        Bài toán: Cho một mảng, tìm phần tử lớn nhất trong mảng.
+        Giải pháp: Sử dụng Linear Search để tìm phần tử lớn nhất.
+        VD: arr = [1, 3, 7, 8, 5, 6, 2]
+
+    Bài 4: Tìm kiếm phần tử nhỏ nhất trong mảng
+        Bài toán: Cho một mảng, tìm phần tử nhỏ nhất trong mảng.
+        Giải pháp: Sử dụng Linear Search để tìm phần tử nhỏ nhất.
+        VD: arr = [1, 3, 7, 8, 5, 6, 2]
+"""
